69d33aba364f7e46fc459327493eb1269329cc4a
salemkingzw <85519657+salemkingzw@users.noreply.github.com>
2025-08-24 12:48:41 +0200

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -162,7 +162,7 @@ os.path.join(BASE_DIR, "static"),
 ]
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/e_mall/')
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media/e_mall/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
